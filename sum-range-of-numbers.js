913e7d0f06623671a442e919f003dcd2ea7c516e
Anker <ankerpeet@gmail.com>
2017-05-30 17:53:27 -0600

    Update sum-range-of-numbers.js

--- sum-range-of-numbers.js
+++ sum-range-of-numbers.js
@@ -1,1 +1,17 @@
+function sumAll(arr) {
+  var newArr = [];
+  var i;
+  var lastArr = [];
 
+function sortNumber(a,b) {
+    return a - b;
+  } newArr = arr.sort(sortNumber);
+  
+  for (i = newArr[0]; i < newArr[1] + 1; i++) {
+    lastArr.push(i);
+  }
+  
+  return lastArr.reduce((a, b) => a + b, 0);
+}
+
+sumAll([12, 4]);
